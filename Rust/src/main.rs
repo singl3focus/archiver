b978e52190b759597b3003808d90c8d96d647c8c
Tursunov Imran <tursunov.imran@mail.ru>
2024-10-28 00:40:27 +0300

    feat: update readme, add format to app, and ref func results

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -2,16 +2,19 @@ extern crate clap;
 
 use clap::{Arg, Command};
 use std::path::PathBuf;
+use std::path::Path;
 use zip_archive::Archiver;
 
-const APP_NAME: &str = "antim";
-const APP_VER: &str = "0.2.0";
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+const APP_NAME: &str = "antim";
+const APP_VER: &str = "0.3.0";
 
 fn main() {
     let mode_arg: &str = "mode";
     let src_arg: &str = "src";
     let dst_arg: &str = "dst";
+    let format_arg: &str = "format";
 
     let app = Command::new(APP_NAME)
         .version(APP_VER)
@@ -39,50 +42,103 @@ fn main() {
                 .required(true)
                 .help("Destination path of compressed file"),
         )
+        .arg(
+            Arg::new(format_arg)
+                .short('f')
+                .long("format")
+                .required(false)
+                .value_parser(["zip"])
+                .help("Data format. Needed only for compressing data.
+                        In case there is a decompression, data format will be define automatically"),
+        )
         .get_matches();
 
-    let mode = app.get_one::<String>(mode_arg).unwrap();
-    let src = app.get_one::<String>(src_arg).unwrap();
-    let dst = app.get_one::<String>(dst_arg).unwrap();
-
+    let mode: &String = app.get_one::<String>(mode_arg).unwrap();
+    let src: &String = app.get_one::<String>(src_arg).unwrap();
+    let dst: &String = app.get_one::<String>(dst_arg).unwrap();
+    
+    let default: &String = &String::from("");
+    let dataformat: &String = app.get_one::<String>(format_arg).unwrap_or(default);
+    
     if src.is_empty() {
         eprintln!("Error: Source path cannot be empty.");
         std::process::exit(1);
     }
     if dst.is_empty() {
-        eprintln!("Error: Source path cannot be empty.");
+        eprintln!("Error: Destination path cannot be empty.");
         std::process::exit(1);
     }
+
     
-    match mode.to_lowercase().as_str() {
-        "compress" => compress_to_zip(src, dst),
-        "decompress" => decompress_from_zip(src, dst),
+    let mode: String = mode.to_lowercase();
+    match mode.as_str() {
+        "compress" => {
+            match compression_distribution(src, dst, dataformat) {
+                Ok(_) => println!("Compress data has been successful."),
+                Err(e) => eprintln!("Compress data has been unsuccessful: {e}."),
+            } 
+        }
+        "decompress" => {
+            match decompression_distribution(src, dst) {
+                Ok(_) => println!("Decompress data has been successful."),
+                Err(e) => eprintln!("Decompress data has been unsuccessful: {e}."),
+            }
+        }
         _ => {
-            eprintln!("Error: Unsupported mode '{}'. Use 'compress' or 'decompress'.", mode);
+            eprintln!("Error: Unsupported mode '{}'.", mode);
             std::process::exit(1);
         }
     }
+    
 }
 
-fn compress_to_zip(src_path: &str, dst_path: &str) {
+// distribution_by_compression_format
+fn compression_distribution(src_path: &str, dst_path: &str, dataformat: &String) -> Result<()> {
+    if dataformat.is_empty() {
+        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Data format is empty")));
+    }
+    
+    match dataformat.to_lowercase().as_str() {
+        "zip" => compress_to_zip(src_path, dst_path),
+        _ => Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Undefined dataformat")))
+    }
+}
+
+fn compress_to_zip(src_path: &str, dst_path: &str) -> Result<()> {
     let src: PathBuf = PathBuf::from(src_path);
     let dest: PathBuf = PathBuf::from(dst_path);
 
     // let thread_count: i32 = 4;
+    
+    let mut archiver: Archiver = Archiver::new();
+    archiver.push(src);        // Add dir to queue
+    archiver.set_destination(dest);  // Add dst path
+    // archiver.set_thread_count(thread_count);
 
-    let mut archiver = Archiver::new();
-    archiver.push(src); // Add dir to queue
-    archiver.set_destination(dest); // Add dst path
-                                    // archiver.set_thread_count(thread_count);
-
-    match archiver.archive() {
-        Ok(_) => println!("Archive the src has been successfull! {}", src_path),
-        Err(e) => println!("Cannot archive the directory! {}", e),
-    };
+    archiver.archive()
 }
 
-fn decompress_from_zip(src_path: &str, dst_path: &str) {
-    // Эта функция должна быть реализована для разжатия zip-архива
-    // println!("Decompressing from {} to {}", src_path, dst_path);
-    eprintln!("Error: Not implemented 'decompress_from_zip', args: [{}] [{}]", src_path, dst_path);
+fn get_file_extension(filename: &str) -> Option<&str> {
+    Path::new(filename).extension()
+        .and_then(|ext| ext.to_str()) // Преобразуем в строку
+        // .map(|ext| ext.to_string()) // Возвращаем расширение как String
+}
+
+// distribution_by_decompression_format
+fn decompression_distribution(src_path: &str, dst_path: &str) -> Result<()> {
+    match get_file_extension(src_path) {
+        Some(ext) => {
+            match ext {
+                "zip" => decompress_from_zip(src_path, dst_path),
+                _ => Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Undefined dataformat")))
+            }
+        },
+        None => Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Extension not found")))
+    }
+}
+
+fn decompress_from_zip(src_path: &str, dst_path: &str) -> Result<()> {
+    _ = src_path;
+    _ = dst_path;
+    Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Not implemented")))
 }
